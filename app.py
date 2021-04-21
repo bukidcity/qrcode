08b20bba07fa2af4cf68aa1fe1e0ce9245ccfbdf
bukidcity <66998266+bukidcity@users.noreply.github.com>
2021-04-21 14:54:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-from flask import Flask, render_template, 
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def scanner():
 	return render_template('html5-qrcode.html')
-
 
 if __name__ == '__main__':
 	#app.run( debug=True)
